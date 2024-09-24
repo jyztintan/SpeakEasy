79879977cb3bd7c25b7c7ad59a17943c1b566e2f
jyztintan <justintanwk2001@gmail.com>
2024-09-24 17:54:01 +0800

    fix: Vercel pls work

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -58,10 +58,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:5173",  # React frontend
-    "https://speakeasy-speakfreely.netlify.app",
-]
+CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # React frontend
